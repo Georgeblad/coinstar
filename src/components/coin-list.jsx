96fa2086641ea8f084a7842f7886b21ef535c1a3
Jorge Muñoz <georgeblad.code@gmail.com>
2022-01-07 17:33:42 -0300

    added loading state

--- src/components/coin-list.jsx
+++ src/components/coin-list.jsx
@@ -1,28 +1,43 @@
-import React, { useContext, useEffect, useState } from 'react'
-import coinGecko from '../apis/coingecko'
-import { WatchListContext } from '../context/watchlist-context'
+import React, { useContext, useEffect, useState } from "react";
+import coinGecko from "../apis/coingecko";
+import { WatchListContext } from "../context/watchlist-context";
+import Coin from "./coin";
 
 const CoinList = () => {
-    const [coins, setCoins] = useState([])
-    const {watchList} = useContext(WatchListContext)
-    console.log(watchList)
-    useEffect(() => {
-        const fetchData = async () => {
-            const response = await coinGecko.get("/coins/markets", {
-                params: {
-                    vs_currency: "usd",
-                    ids: watchList.join(","),
-                }
-            })
-            console.log(response.data)
-        }
-        fetchData()
-    }, [])
-    return (
-        <div>
-            
-        </div>
-    )
-}
+  const [coins, setCoins] = useState([]);
+  const { watchList } = useContext(WatchListContext);
+  const [isLoading, setIsLoading] = useState(false);
 
-export default CoinList
+  useEffect(() => {
+    const fetchData = async () => {
+      setIsLoading(true);
+      const response = await coinGecko.get("/coins/markets", {
+        params: {
+          vs_currency: "usd",
+          ids: watchList.join(","),
+        },
+      });
+      setCoins(response.data)
+      setIsLoading(false)
+    };
+    fetchData();
+  }, []);
+
+
+  const renderCoins = () => {
+      if(isLoading) {
+          return <div>Loading...</div>
+      }
+      return (
+          <ul className="coinlist list-group mt-2">
+            {coins.map(coin => {
+                return <Coin key={coin.id} coin={coin} />
+            })}
+          </ul>
+      )
+  }
+
+  return <div>{renderCoins()}</div>;
+};
+
+export default CoinList;
