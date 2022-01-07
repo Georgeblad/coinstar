8f0850e42bbb75fb13263b8e8c7368f47ef8455f
Jorge Muñoz <georgeblad.code@gmail.com>
2022-01-07 18:18:50 -0300

    conditional coin volume 24H

--- src/components/coin.jsx
+++ src/components/coin.jsx
@@ -1,17 +1,27 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import React from "react";
 import { Link } from "react-router-dom";
-import { faSortDown } from "@fortawesome/free-solid-svg-icons";
+import { faSortDown, faSortUp } from "@fortawesome/free-solid-svg-icons";
 
 const Coin = ({ coin }) => {
   return (
     <Link to="/coindetail">
-      <li className="coinlist-item p-3 bg-slate-100 my-px rounded-md shadow-md flex justify-between items-center text-gray-900">
+      <li className="coinlist-item p-3 bg-slate-50 my-px rounded-md shadow-md flex justify-between items-center text-gray-900 hover:bg-slate-200">
         <img className="coinlist-image" alt="asd" src={coin.image} />
         <span>{coin.current_price}</span>
-        <span className="mr-2 text-green-500">
+        <span
+          className={
+            coin.price_change_percentage_24h < 0
+              ? "text-red-500 mr-2"
+              : "text-green-400 mr-2"
+          }
+        >
+          {coin.price_change_percentage_24h < 0 ? (
             <FontAwesomeIcon icon={faSortDown} className="mr-2" />
-            {coin.price_change_percentage_24h}
+          ) : (
+            <FontAwesomeIcon icon={faSortUp} className="mr-2" />
+          )}
+          {coin.price_change_percentage_24h}
         </span>
         <i className="delete-icon far fa-times-circle text-red-500"></i>
       </li>
